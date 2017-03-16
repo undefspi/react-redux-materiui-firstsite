82554ca6a4271793a736d9a878c17d32f24737fd
undefspi <chris.beech@wavelytics.co.uk>
2017-03-16 17:54:57 +0000

    Initial Working Base Application

---
+++ node/main/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+var App = React.createClass({
+  render(){
+    return(
+      <div>
+        <h1>Welcome, lets explore some material-ui</h1>
+      </div>
+    );
+  }
+})
+
+export default App;
--- node/webpack.config.js
+++ node/webpack.config.js
@@ -8,7 +8,7 @@ var config = {
 
   devServer:{
     inline: true,
-    port: 8080
+    port: 9000
   },
 
   module:{
